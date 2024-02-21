36bc7e859ea515d1181e90aca705618bf6212ef5
MaroanMejric <89808411+MaroanMejric@users.noreply.github.com>
2024-02-21 09:01:50 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const schedule = require('node-schedule');
-const generalController = require('./generalController');
-
-generalController.executeRoutineTasks();
-
-schedule.scheduleJob('0 8 * * *', () => {                     //ogni giorno alle 8 di mattina esegue la sua routine
-    console.log('ROUTINE STARTED AT 08:00');
-    generalController.executeRoutineTasks();
-});
